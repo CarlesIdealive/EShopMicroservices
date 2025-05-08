026411c5ae33fe32676a1066e051c8a38c1fd85a
Carles Labrana <carles.labrana@idealiveconsulting.com>
2025-05-08 19:16:17 +0200

    CQRS: Interfaces in BuildingBlocks Project

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    // ICommand que no produce una respuesta
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    // ICommand que produce una respuesta
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+
+
+}
