e4dc3ed93e1236d07e3df8d1d3d9c113e4f389c7
Carles Labrana <carles.labrana@idealiveconsulting.com>
2025-05-21 12:32:20 +0200

    Implementación de servicio gRPC para gestión de cupones
    Se creó el servicio gRPC `DiscountService` con métodos para gestionar
    cupones (`GetDiscount`, `CreateDiscount`, `UpdateDiscount`,
    `DeleteDiscount`) definidos en `discount.proto`. Se utilizó SQLite
    como base de datos con migraciones iniciales y datos predefinidos.
    
    Cambios principales:
    - Clase `DiscountContext` para manejar la base de datos.
    - Clase `Coupon` como modelo de datos.
    - Migraciones iniciales para la tabla `Coupons`.
    - Extensión `UseMigration` para aplicar migraciones automáticas.
    - Configuración de servicios gRPC y base de datos en `Program.cs`.
    - Uso de `Mapster` para mapeo entre entidades y modelos gRPC.
    - Configuración de inicio en `launchSettings.json` y ajustes en
      `appsettings.Development.json`.
    - Actualización del archivo de proyecto para usar .NET 9.0 y nuevos
      paquetes.
    
    La aplicación está lista para ejecutarse localmente o en Docker.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var services = scope.ServiceProvider;
+            using var dbContext = services.GetRequiredService<DiscountContext>();
+            try
+            {
+                dbContext.Database.MigrateAsync();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating the DB");
+            }
+            return app;
+        }
+
+
+    }
+
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,87 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
+    {
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+            if (coupon is null)
+                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount" };
+            //LoggInformation
+            logger.LogInformation("Discount Service: GetDiscount: {ProductName} - {Amount}", coupon.ProductName, coupon.Amount);
+
+            //Transforma el objeto Coupon para CouponModel
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid required coupon"));
+
+            await dbContext.Coupons.AddAsync(coupon);
+            var success = await dbContext.SaveChangesAsync();
+            if (success == 0)
+                throw new RpcException(new Status(StatusCode.Internal, "Error creating coupon"));
+            //LoggInformation
+            logger.LogInformation("Discount Service: CreateDiscount: {ProductName} - {Amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+
+        }
+
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid required coupon"));
+
+            dbContext.Coupons.Update(coupon);
+            var success = await dbContext.SaveChangesAsync();
+            if (success== 0)
+                throw new RpcException(new Status(StatusCode.Internal, "Error updating coupon"));
+
+            //LoggInformation
+            logger.LogInformation("Discount Service: UpdateDiscount: {ProductName} - {Amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+
+        }
+
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"Coupon with name {request.ProductName}, not Found"));
+
+            dbContext.Coupons.Remove(coupon);
+            await dbContext.SaveChangesAsync();
+
+            //LoggInformation
+            logger.LogInformation("Discount Service: DeleteDiscount: {ProductName} Deleted", coupon.ProductName);
+
+            return new DeleteDiscountResponse { Success = true };
+
+        }
+
+
+
+
+    }
+
+}
